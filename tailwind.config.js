1ad8f557a922974c4729f29770fe98f336131d3d
Jeff Temes <jefftemes@gmail.com>
2021-07-26 20:05:14 -0700

    Commented out purge for testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,8 +27,8 @@ module.exports = {
   },
   variants: {},
   plugins: [],
-  purge: process.env.NODE_ENV === 'production' ? {
-    enabled: true,
-    content: ['src/**/*.njk', 'src/**/*.js'],
-  } : {}
+  // purge: process.env.NODE_ENV === 'production' ? {
+  //   enabled: true,
+  //   content: ['src/**/*.njk', 'src/**/*.js'],
+  // } : {}
 }
